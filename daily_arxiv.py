99c53ae08128232598cdac2331655123402643de
Jie Liu <33612777+keeper-jie@users.noreply.github.com>
2026-05-28 09:46:37 +0800

    Simplify result retrieval from search engine
    Refactor to use search_engine directly for results.

--- daily_arxiv.py
+++ daily_arxiv.py
@@ -98,9 +98,7 @@ def get_daily_papers(topic,query="slam", max_results=2):
         sort_by = arxiv.SortCriterion.SubmittedDate
     )
 
-    # v2.x 需要创建Client对象来执行搜索
-    client = arxiv.Client()
-    for result in client.results(search_engine):
+    for result in search_engine.results():
 
         paper_id            = result.get_short_id()
         paper_title         = result.title
